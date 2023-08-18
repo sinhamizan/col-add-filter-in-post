9147efb9e0dcc9c887484b224c61ecb5da5a7997
Mizanur Rahaman <mizan88developer@gmail.com>
2023-08-18 16:42:48 +0100

    Start work

---
+++ column-management.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name:       Column Management
+ * Plugin URI:        https://github.com/sinhamizan/col-add-filter-in-post  
+ * Description:       Add QR Code in your posts or pages.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mizanur Rahaman
+ * Author URI:        https://github.com/sinhamizan
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/sinhamizan/col-add-filter-in-post
+ * Text Domain:       column-management
+ * Domain Path:       /languages
+ */
+
+
